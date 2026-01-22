048b791e30b60ceefe3ab041d6c3575b1c6f50c3
Md Arman <armanar6712@gmail.com>
2026-01-22 08:32:16 +0530

    feat: add avatar component with fallback and image support

--- src/components/layout/BaseHeader.vue
+++ src/components/layout/BaseHeader.vue
@@ -1,7 +1,34 @@
+<script setup lang="ts">
+import BaseAvatar from '@/components/base/BaseAvatar.vue'
+import { Button } from '@/components/ui/button'
+
+import { RouterLink } from 'vue-router'
+
+import { ArrowUpRight, Router } from 'lucide-vue-next'
+
+const githubUrl = 'https://github.com/itsarman001/driverjs-example'
+
+const openExternalLink = (url: string) => {
+  window.open(url, '_blank')
+}
+</script>
+
 <template>
-  <header>
-    <nav class="bg-gray-800 p-4 text-white">
-      <h1 class="text-2xl font-bold">My Application Header</h1>
+  <header class="flex items-center justify-between p-4 border-b">
+    <RouterLink to="/" class="flex items-center gap-2">
+      <BaseAvatar />
+      <span class="text-xl">dev.portfolio</span>
+    </RouterLink>
+
+    <nav>
+      <ul>
+        <li>
+          <Button variant="ghost" @click="openExternalLink(githubUrl)" class="flex items-center">
+            <span>GitHub</span>
+            <ArrowUpRight />
+          </Button>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
