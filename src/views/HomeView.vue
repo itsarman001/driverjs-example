4dd4fe6489212aa07a077a52056dc618724c9fcf
Md Arman <armanar6712@gmail.com>
2026-01-23 23:59:21 +0530

    feat: update HomeView with new layout and buttons
    - Replaced the existing header with a more descriptive title and paragraph.
    - Added two buttons for user interaction: "Get in Touch" and "Lets Talk".
    - Included an illustration to enhance the visual appeal of the page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <main class="flex items-center justify-center">
-    <h1 class="text-4xl font-bold">Hello, World</h1>
+    <section>
+      <div>
+        <h1 class="text-4xl font-bold">creative. developer</h1>
+        <p>
+          Building delightful digital experiences with clean code, intuitive design, and a focus on
+          user happiness.
+        </p>
+      </div>
+      <div>
+        <Button size="lg">Get in Touch</Button>
+        <Button variant="outline" size="lg">Lets Talk</Button>
+      </div>
+    </section>
+    <section class="h-20 w-20">
+      <img src="/developer.svg" alt="developer illustration" />
+    </section>
   </main>
 </template>
